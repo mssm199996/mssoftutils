33fe394861f20e1ff6d9b4326a03f426c1aedbd1
sidi mohamed moulay-slimane <mssm199996@gmail.com>
2021-02-21 14:01:42 +0100

    Added rest api search entity by compte id

---
+++ src/main/java/mssoftutils/api/dto/CompteIdOffsetSearchRequestDto.java
@@ -0,0 +1,14 @@
+package mssoftutils.api.dto;
+
+public class CompteIdOffsetSearchRequestDto extends OffsetSearchRequestDto {
+
+    private int idCompte;
+
+    public int getIdCompte() {
+        return idCompte;
+    }
+
+    public void setIdCompte(int idCompte) {
+        this.idCompte = idCompte;
+    }
+}
